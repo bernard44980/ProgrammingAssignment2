a4515b66ce2215c4ed16db144444a4bff7dac66d
bernard44980 <bernard.offmann@univ-nantes.fr>
2014-08-25 01:04:46 +0200

    3rd commit with comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,16 +2,28 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   
-  #This script is done by bernard44980
-  #Date : 20140822
-  inverse <- NULL
+  #This creates a special matrix which should contain in fact
+  #a list of 4 functions
+
+  inverse <- NULL  #this initialize the object that will store the inverse matrix
+  
+  #The first function called "set" will set the value of the matrix
   set <- function(y) {
     x <<- y
     inverse <<- NULL
   }
+  
+  #the second function called "get" will get the value of the matrix
   get <- function() x
+  
+  #the third function called "setinverse" will set the value of the inverse of the matrix
+  # it uses the function solve
   setinverse <- function(solve) inverse <<- solve
+  
+  #the fourth function called "getinverse" will get the value of the inverse of the matrix
   getinverse <- function() inverse
+  
+  #below is the instruction that returns a list of the 4 functions defined above
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
@@ -21,8 +33,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-  inverse <- x$getinverse()
-  if(!is.null(m)) {
+  inverse <- x$getinverse() #this does not work : "Error in x$getinverse : $ operator is invalid for atomic vectors"
+  if(!is.null(inverse)) {
     message("getting cached data")
     return(inverse)
   }
